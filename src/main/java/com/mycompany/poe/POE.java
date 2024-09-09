e11e467e6486b519527bb9f9b17b60630a6f7b20
RC_Student_lab <RC_Student_lab@10.0.1.23>
2024-09-09 08:23:12 +0000

    added password variable

--- src/main/java/com/mycompany/poe/POE.java
+++ src/main/java/com/mycompany/poe/POE.java
@@ -15,7 +15,7 @@ public class POE {
     public static void main(String[] args) {
        
        String  username = "Tim_";
-       String password = "";
+       String password = "Cs557mole";
        
        if(username.contains("_") && username.length()<5){
            System.out.println("username successfully captured");
@@ -24,33 +24,7 @@ public class POE {
                    + "contains an underscore and the length is no more than 5 characters long.");
        }
        
-       //gdg
-       
-       //this method returns a string charcter in a particular position
-      /* char results = hello.charAt(17);
-       System.out.println(results);*/
-       
-       /*contains() --this method return a boolean(True/False) since it checks if a 
-       string contains a certain character or not.*/
-       //System.out.println(hello.contains("le"));
-       
-       /*length()  ---this method determines the length of tth string and 
-       it returns an integer*/
-      // System.out.println(hello.length());
-       
-       //compareTo() --this method returns an int
-       
-      // System.out.println(hello.compareToIgnoreCase(compare));
-       
-       /*endsWith()  ---thi method checks if a string ends with a cetain characters
-       it returns a boolean*/
-      // System.out.println(hello.endsWith("ld"));
-       
-       //concat() it connects two strings into one string
-      // System.out.println(hello.concat(compare));
-      
-      //equals --this method comapres two strings and returns a boolean
-      System.out.println(hello.equals("hello"));
+       ///Are we on Github?
     }
     
 }
